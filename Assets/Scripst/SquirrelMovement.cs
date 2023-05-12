0427a878ae30c05d3730e04c9ecd476fd455ef96
Rostyslav Bobrakov <rbobrakov@office.cdv.pl>
2023-05-12 22:57:06 +0200

    Interaction with the cage.

--- Assets/Scripst/SquirrelMovement.cs
+++ Assets/Scripst/SquirrelMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class SquirrelMovement : MonoBehaviour
@@ -13,6 +14,8 @@ public class SquirrelMovement : MonoBehaviour
     public float jumpForce = 10f;
     public float glidingSpeed;
     private float _initialGravityScale;
+    private GameObject keyObject = null;
+    public BoxCollider2D rightCollider;
 
     public LayerMask collisionMask;
 
@@ -24,16 +27,13 @@ public class SquirrelMovement : MonoBehaviour
 
     void Update()
     {
-
         var inputX = Input.GetAxisRaw("Horizontal");
 
         if (inputX == 0)
         {
             Animations.SetBool("IsRunning", false);
         }
-
         else
-
         {
             Animations.SetBool("IsRunning", true);
         }
@@ -47,7 +47,6 @@ public class SquirrelMovement : MonoBehaviour
             _rigidbody.gravityScale = 0;
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, -glidingSpeed);
         }
-
         else
         {
             Animations.SetBool("Fly", false);
@@ -60,6 +59,7 @@ public class SquirrelMovement : MonoBehaviour
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
         }
+
         if (inputX != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(inputX), 1, 1);
@@ -69,5 +69,33 @@ public class SquirrelMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionMask) != null;
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Key") && keyObject == null)
+        {
+            keyObject = other.gameObject;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Cage") && keyObject != null && rightCollider.enabled)
+        {
+            other.gameObject.GetComponent<Animator>().SetTrigger("Cage");
+            keyObject.SetActive(false);
+            keyObject = null;
+            rightCollider.enabled = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cage") && keyObject != null && rightCollider.enabled)
+        {
+            collision.gameObject.GetComponent<Animator>().SetTrigger("Cage");
+            keyObject.SetActive(false);
+            keyObject = null;
+            rightCollider.enabled = false;
+        }
     }
 }
