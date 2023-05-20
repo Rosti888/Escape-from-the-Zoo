302af030643860bd88453ba3659e8530ab38be41
Rostyslav Bobrakov <rbobrakov@office.cdv.pl>
2023-05-20 21:00:54 +0200

    Background City.

--- Assets/Scripst/Swimming.cs
+++ Assets/Scripst/Swimming.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Swimming : MonoBehaviour
 {
     private Animator Animations;
+    private new CapsuleCollider2D collider;
+    private float originalSizeX;
+    private float originalSizeY;
+    private Vector2 originalOffset;
 
     void Start()
     {
         Animations = GetComponentInChildren<Animator>();
+        collider = GetComponent<CapsuleCollider2D>();
+
+        originalSizeX = collider.size.x;
+        originalSizeY = collider.size.y;
+        originalOffset = collider.offset;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -16,11 +25,15 @@ public class Swimming : MonoBehaviour
         if (collision.gameObject.CompareTag("WaterZone"))
         {
             Animations.SetBool("IsSwimming", true);
+            collider.size = new Vector2(1.660827f, 0.24444f); 
+            collider.offset = new Vector2(0.3223473f, 0.11550f); 
         }
 
         if (collision.gameObject.CompareTag("Ground"))
         {
             Animations.SetBool("IsSwimming", false);
+            collider.size = new Vector2(1.660827f, 0.764586f); 
+            collider.offset = new Vector2(0.3223473f, 0.375589f); 
         }
     }
 }
