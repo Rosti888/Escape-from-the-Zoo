5c7b6c5ad195d7cf01ff4fbd37324c62f1bbf9d8
Artsiom Liakhouski <aliakhouski@edu.cdv.pl>
2023-05-25 14:36:18 +0200

    FixButton

--- Assets/Scripst/ButtonPlatform.cs
+++ Assets/Scripst/ButtonPlatform.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonPlatform : MonoBehaviour
 {
-    public GameObject button;
+    public GameObject buttonOff1;
+    public GameObject buttonOff2;
     public GameObject platform;
     public Transform pointStart, pointEnd;
     public float speed;
+    public bool isButton1Pressed;
+    public bool isButton2Pressed;
 
     private Vector2 targetPos;
 
@@ -19,29 +20,84 @@ public class ButtonPlatform : MonoBehaviour
     void Update()
     {
         platform.transform.position = Vector2.MoveTowards(platform.transform.position, targetPos, speed * Time.deltaTime);
+
+        if (isButton1Pressed || isButton2Pressed)
+        {
+            targetPos = pointEnd.position;
+        }
+        else
+        {
+            targetPos = pointStart.position;
+        }
+
+        if (isButton1Pressed)
+        {
+            buttonOff1.SetActive(false);
+        }
+        else
+        {
+            buttonOff1.SetActive(true);
+        }
+
+        if (isButton2Pressed)
+        {
+            buttonOff2.SetActive(false);
+        }
+        else
+        {
+            buttonOff2.SetActive(true);
+        }
     }
 
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //targetPos = pointEnd.position;
+            isButton1Pressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isButton1Pressed = false;
+
+        }
+    }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //targetPos = pointEnd.position;
+            isButton2Pressed = true;
+
+        }
+    }
+
+
+    /*
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            targetPos = pointEnd.position;
-            button.SetActive(false);
-        }
-
-        else
-        {
-            targetPos = pointStart.position;
-            button.SetActive(true);
+            //targetPos = pointEnd.position;
+            isButton1Pressed = true;
         }
     }
+    */
 
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            targetPos = pointStart.position;
-            button.SetActive(true);
+            isButton2Pressed = false;
+            buttonOff2.SetActive(true);
         }
     }
 }
