aea1e72f148cf9affa3ca0f433467b7e18bbe455
Mar1 <mholik@edu.cdv.pl>
2023-05-25 03:06:05 +0200

    TreeScriptAndImage

---
+++ Assets/Sandbox/Maryia/TreeClimbing.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+public class TreeClimbing : MonoBehaviour
+{
+    private float vertical;
+    public float speed;
+    private bool isLadder;
+    private bool isClimbing;
+
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (isLadder && Mathf.Abs(vertical) > 0f)
+        {
+            isClimbing = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, vertical * speed);
+        }
+        else
+        {
+            rb.gravityScale = 4f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Tree"))
+        {
+            isLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Tree"))
+        {
+            isLadder = false;
+            isClimbing = false;
+        }
+    }
+}
