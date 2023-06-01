9473b9410ef956e834cab31437e84799c45ec13b
Rostyslav Bobrakov <rbobrakov@edu.cdv.pl>
2023-06-01 17:44:17 +0200

    Only FullHD.

---
+++ Assets/Scripst/FullHD.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullHD : MonoBehaviour
+{
+    void Start()
+    {
+        
+        Screen.SetResolution(1920, 1080, FullScreenMode.ExclusiveFullScreen);
+    }
+}
